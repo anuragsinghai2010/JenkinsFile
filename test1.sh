b3aa7876f46832d22619077a5cbcee7fab580f3a
anuragsinghai2010 <anuragsinghai2010@gmail.com>
2021-12-30 22:59:11 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,3 +1,3 @@
 echo "welcome to my second shell script"
-echo "location start time is $location_start_time and location end time is $2"
-echo "product start time is $3 and product end time is $4"
+echo ''' +location_start_time+ '''
+
