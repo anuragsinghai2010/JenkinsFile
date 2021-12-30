aa81812b97864c5106e69ca4812067f8fde1e1fb
anuragsinghai2010 <anuragsinghai2010@gmail.com>
2021-12-30 22:03:25 +0530

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,3 @@
+echo "welcome to my second shell script"
+echo "location start time is $location_start_time and location end time is $location_end_time"
+echo "product start time is $product_start_time and product end time is $product_end_time"
