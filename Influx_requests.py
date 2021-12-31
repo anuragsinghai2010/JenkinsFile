3c89456d27df68ad9000c099b0796f6a652b3bcb
anuragsinghai2010 <anuragsinghai2010@gmail.com>
2021-12-31 22:49:23 +0530

    Update Influx_requests.py

--- Influx_requests.py
+++ Influx_requests.py
@@ -1,5 +1,5 @@
 import requests
-sys.path.append(os.environ['WORKSPACE'])
+
 
 response = requests.get("http://localhost:8086/query?db=example&q=select * from cpu_load_short where time>= 1640991590000000000 and time<=1640991670000000000")
 print(response.text)
