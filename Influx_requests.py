4bc6b54415fb8d7a3dd29b3a08192ec22a8b2a78
anuragsinghai2010 <anuragsinghai2010@gmail.com>
2021-12-31 19:27:13 +0530

    Add files via upload

---
+++ Influx_requests.py
@@ -0,0 +1,4 @@
+import requests
+
+response = requests.get("http://localhost:8086/query?db=example&q=select * from cpu_load_short where time>= 1640991590000000000 and time<=1640991670000000000")
+print(response.text)
