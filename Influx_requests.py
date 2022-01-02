9ea4fab75e8990fd8591ea27685f07d54647394e
anuragsinghai2010 <anuragsinghai2010@gmail.com>
2022-01-02 12:45:36 +0530

    Update Influx_requests.py

--- Influx_requests.py
+++ Influx_requests.py
@@ -1,5 +1,5 @@
 import requests
 
 
-response = requests.get("http://localhost:8086/query?db=example&q=select * from cpu_load_short where time>= 1640991590000000000 and time<=1640991670000000000")
+response = requests.get("http://localhost:8086/query?db=example&q=select * from cpu_load_short where time>="+sttime+" and "time<= "+endtime+")
 print(response.text)
