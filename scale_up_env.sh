88224c7be1301a6cb359fa444f444ef51e4741a7
anuragsinghai2010 <anuragsinghai2010@gmail.com>
2022-01-06 13:14:14 +0530

    Update scale_up_env.sh

--- scale_up_env.sh
+++ scale_up_env.sh
@@ -1,3 +1,6 @@
 echo "welcome to my first shell script"
 start=`date +%s`
 echo "current time is ${start}"
+STATUS=`gcloud compute instances describe --project "saas-non-prod-infra-220605" --zone "us-central1-a" "petco-perftest-cassandra-01" | grep status | tr -s ' ' | awk '{print $2}'`
+
+echo $STATUS
